1e187d5a17ebc93b047ae093706bd719408b5875
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-01-09 09:55:49 +0530

    Initial Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='text-red-600'>Shyam Kumar Sharma</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
