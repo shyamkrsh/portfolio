91800b0ca96079c8e146f8cf1f45430b126b0c7f
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-01-13 20:47:47 +0530

    btn styled

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 
 
-function ProjectCard({image, title, link}) {
+function ProjectCard({ image, title, link }) {
     return (
         <div className='projectCard bg-slate-700 rounded-md w-[90%] py-3 flex items-center flex-col cursor-pointer  m-auto'>
             <div className='img w-[90%] h-[11rem] mt-3 rounded-md'>
@@ -13,8 +13,8 @@ function ProjectCard({image, title, link}) {
                 <h1 className='text-xl font-semibold text-center mt-3'>{title}</h1>
             </div>
             <div className='mt-3 flex items-center gap-5'>
-                <button className='' onClick={() => location.href=link}><FaGithub className='text-3xl' /></button>
-                <button className='' onClick={() => location.href=link}><FaArrowUpRightFromSquare className='text-2xl' /></button>
+                <button className='menuBtn' onClick={() => location.href = link}><FaGithub className='text-3xl' /></button>
+                <button className='menuBtn' onClick={() => location.href = link}><FaArrowUpRightFromSquare className='text-2xl' /></button>
             </div>
         </div>
     )
