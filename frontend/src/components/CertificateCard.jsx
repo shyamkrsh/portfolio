ed2594f6fa404f1ebe53b2bdde11c65bf282d049
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-01-13 10:20:51 +0530

    framer motion added

--- frontend/src/components/CertificateCard.jsx
+++ frontend/src/components/CertificateCard.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { motion } from "motion/react"
+import React, { useEffect, useState } from "react";
 import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 
-function CertificateCard({image, title, link}) {
+function CertificateCard({ image, title, link }) {
+    const [inView, setInView] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const element = document.getElementById("animated-div");
+            const rect = element.getBoundingClientRect();
+            const windowHeight = window.innerHeight;
+            if (rect.top <= windowHeight && rect.bottom >= -300) {
+                setInView(true);
+            } else {
+                setInView(false);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
     return (
-        <div className='bg-slate-900 rounded-md w-[20rem] h-[20rem] flex items-center flex-col cursor-pointer'>
+        <motion.div className='bg-slate-900 rounded-md w-[20rem] h-[20rem] flex items-center flex-col cursor-pointer'
+            initial={{ opacity: 0, scale: 0 }}
+            animate={inView ? { opacity: 1, scale: 1 } : { opacity: 1, scale: 1 }}
+            transition={{
+                duration: 0.4,
+                scale: {  type: "spring", visualDuration: 0.4, bounce: 0.5 },
+            }}
+        >
             <div className='img w-[18rem] h-[11rem] mt-3'>
                 <img src={image} className='w-[100%] h-[100%] rounded-md' />
             </div>
@@ -11,9 +35,9 @@ function CertificateCard({image, title, link}) {
                 <h1 className='text-xl font-semibold text-center mt-3'>{title}</h1>
             </div>
             <div className='mt-5'>
-                <button className='text-white bg-green-600 hover:bg-green-500 px-3 py-2 rounded-md font-semibold flex items-center justify-center gap-2' onClick={() => location.href=link}>View Certificate<FaArrowUpRightFromSquare className='text-xl' /></button>
+                <button className='text-white bg-green-600 hover:bg-green-500 px-3 py-2 rounded-md font-semibold flex items-center justify-center gap-2' onClick={() => location.href = link}>View Certificate<FaArrowUpRightFromSquare className='text-xl' /></button>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
--- frontend/src/components/Education.jsx
+++ frontend/src/components/Education.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import { motion } from "motion/react"
+import React, { useEffect, useState } from "react";
 import { ImQuotesLeft } from "react-icons/im";
 import { ImQuotesRight } from "react-icons/im";
 
 function Education() {
+    const [inView, setInView] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const element = document.getElementById("animated-div");
+            const rect = element.getBoundingClientRect();
+            const windowHeight = window.innerHeight;
+            if (rect.top <= windowHeight && rect.bottom >= 0) {
+                setInView(true);
+            } else {
+                setInView(false);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
         <div className='w-[100%] education '>
             <h1 className='text-4xl ms:text-5xl text-center font-bold pt-20'>𝔼𝕕𝕦𝕔𝕒𝕥𝕚𝕠𝕟 </h1>
             <div className='flex flex-col md:flex-row items-center justify-center gap-20 mt-10'>
-                <div className='bg-slate-900 p-2 rounded-md w-[90%] md:w-[30%] h-[30vh] relative'>
+                <motion.div className='bg-slate-900 p-2 rounded-md w-[90%] md:w-[30%] h-[30vh] relative' id="animated-div"
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={inView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0 }}
+                    transition={{
+                        duration: 0.4,
+                        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+                    }}
+                >
                     <h2 className=' text-slate-600'><ImQuotesLeft className='text-2xl' /></h2>
                     <div>
                         <h1 className='mt-3 text-2xl font-semibold text-center'>Diploma in Computer Science</h1>
@@ -15,9 +40,16 @@ function Education() {
                         <h3 className='text-center font-semibold text-slate-400 mt-1'>CGPA : 8.60</h3>
                         <h2 className='text-center font-semibold text-xl mt-3'>Oct 2022 - Jun 2025</h2>
                     </div>
-                    <h2 className='flex items-center justify-end text-slate-600 absolute bottom-2 right-2'><ImQuotesRight className='text-2xl'/></h2>
-                </div>
-                <div className='bg-slate-900 p-2 rounded-md w-[90%] md:w-[30%] h-[30vh] relative'>
+                    <h2 className='flex items-center justify-end text-slate-600 absolute bottom-2 right-2'><ImQuotesRight className='text-2xl' /></h2>
+                </motion.div>
+                <motion.div className='bg-slate-900 p-2 rounded-md w-[90%] md:w-[30%] h-[30vh] relative'
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={inView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0 }}
+                    transition={{
+                        duration: 0.8,
+                        scale: { type: "spring", visualDuration: 0.8, bounce: 0.5 },
+                    }}
+                >
                     <h2 className=' text-slate-600'><ImQuotesLeft className='text-2xl' /></h2>
                     <div>
                         <h1 className='mt-3 text-2xl font-semibold text-center'>Matric</h1>
@@ -25,8 +57,8 @@ function Education() {
                         <h3 className='text-center font-semibold text-slate-400 mt-1'>Percentage : 90.8</h3>
                         <h2 className='text-center font-semibold text-xl mt-3'>Oct 2022 - Jun 2025</h2>
                     </div>
-                    <h2 className='flex items-center justify-end text-slate-600 absolute bottom-2 right-2'><ImQuotesRight className='text-2xl'/></h2>
-                </div>
+                    <h2 className='flex items-center justify-end text-slate-600 absolute bottom-2 right-2'><ImQuotesRight className='text-2xl' /></h2>
+                </motion.div>
 
             </div>
         </div>
